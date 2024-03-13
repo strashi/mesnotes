db3d9f3cf4ed846380ae5d041a8157c9735fece0
Stève Commer <steve.commer@laposte.net>
2024-03-13 09:12:59 +0100

    Inintial commit

---
+++ src/main/java/xyz/strashi/mesnotes/MesnotesApplication.java
@@ -0,0 +1,13 @@
+package xyz.strashi.mesnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesnotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MesnotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/strashi/mesnotes/MesnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.strashi.mesnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesnotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
